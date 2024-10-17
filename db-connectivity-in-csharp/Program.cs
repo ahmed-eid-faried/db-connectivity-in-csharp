867f08ad98f04b3cb1db0a7992cd61dbca9ecbf0
Ahmed Mady <96249012+ahmed-eid-faried@users.noreply.github.com>
2024-10-17 15:15:53 +0300

    PrintAllContacts2 and database  with .net framwork

--- db-connectivity-in-csharp/Program.cs
+++ db-connectivity-in-csharp/Program.cs
@@ -5,7 +5,8 @@ using System.Data.SqlClient;
 
 public class Program
 {
-    static string connectionString = "Server=.;Database=ContactsDB;User Id=sa;Password=YourStrong!Passw0rd;";
+
+    static string connectionString = "Server=.;Database=ContactsDB;User Id=sa;Password=123456;";
 
     static void PrintAllContacts()
     {
@@ -15,14 +16,12 @@ public class Program
         string query = "SELECT * FROM Contacts";
 
         SqlCommand command = new SqlCommand(query, connection);
-
         try
         {
             connection.Open();
-
             SqlDataReader reader = command.ExecuteReader();
 
-            while (reader.Read()) // to loop for all records
+            while (reader.Read())
             {
                 int contactID = (int)reader["ContactID"];
                 string firstName = (string)reader["FirstName"];
@@ -43,6 +42,7 @@ public class Program
 
             reader.Close();
             connection.Close();
+
         }
 
 
@@ -56,43 +56,39 @@ public class Program
 
     static void PrintAllContacts2()
     {
-
         SqlConnection connection = new SqlConnection(connectionString);
-        const string qeury = "SELECT * FROM Contacts;";
-        SqlCommand command = new SqlCommand(qeury, connection);
-
+        string query = "select * from Contacts";
+        SqlCommand command = new SqlCommand(query, connection);
         try
         {
             connection.Open();
             SqlDataReader reader = command.ExecuteReader();
             while (reader.Read())
             {
-                int ContactID = (int)reader["ContactID"];
-                string FirstName = (string)reader["FirstName"];
-                string LastName = (string)reader["LastName"];
-                string Email = (string)reader["Email"];
-                string Phone = (string)reader["Phone"];
-                string Address = (string)reader["Address"];
-                int CountryID = (int)reader["CountryID"];
+                int contactID = (int)reader["ContactID"];
+                string firstName = (string)reader["FirstName"];
+                string lastName = (string)reader["LastName"];
+                string email = (string)reader["Email"];
+                string phone = (string)reader["Phone"];
+                string address = (string)reader["Address"];
+                int countryID = (int)reader["CountryID"];
 
-                Console.WriteLine("ContactID ===>> " + ContactID);
-                Console.WriteLine("FirstName ===>> " + FirstName);
-                Console.WriteLine("LastName ===>> " + LastName);
-                Console.WriteLine("Email ===>> " + Email);
-                Console.WriteLine("Phone ===>> " + Phone);
-                Console.WriteLine("Address ===>> " + Address);
-                Console.WriteLine("CountryID ===>> " + CountryID);
-                Console.WriteLine("\n\n\n");
-
+                Console.WriteLine($"Contact ID: {contactID}");
+                Console.WriteLine($"Name: {firstName} {lastName}");
+                Console.WriteLine($"Email: {email}");
+                Console.WriteLine($"Phone: {phone}");
+                Console.WriteLine($"Address: {address}");
+                Console.WriteLine($"Country ID: {countryID}");
+                Console.WriteLine();
             }
             reader.Close();
             connection.Close();
-        }
-        catch (Exception error)
-        {
+        } catch (Exception e) {
+            Console.WriteLine("Error: " + e.Message);
 
-            Console.WriteLine("Error: " + error.Message);
         }
+
+
 
     }
     public static void Main()
