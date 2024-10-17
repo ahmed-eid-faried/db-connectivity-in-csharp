3bb213f4e59f3ae89341ea9338a2dc35bacddcad
Ahmed Mady <96249012+ahmed-eid-faried@users.noreply.github.com>
2024-10-17 16:13:27 +0300

    ParameterizedQuery

--- db-connectivity-in-csharp/Program.cs
+++ db-connectivity-in-csharp/Program.cs
@@ -83,7 +83,9 @@ public class Program
             }
             reader.Close();
             connection.Close();
-        } catch (Exception e) {
+        }
+        catch (Exception e)
+        {
             Console.WriteLine("Error: " + e.Message);
 
         }
@@ -91,10 +93,96 @@ public class Program
 
 
     }
+
+    static void ParameterizedQuery(string FirstName, int CountryID)
+    {
+        SqlConnection connection = new SqlConnection(connectionString);
+        //string query = "select * from Contacts where Contacts.FirstName=@FirstName";
+        string query2 = "select * from Contacts where Contacts.FirstName=@FirstName and Contacts.CountryID=@CountryID;";
+        SqlCommand command = new SqlCommand(query2, connection);
+        command.Parameters.AddWithValue("@FirstName", FirstName);
+        command.Parameters.AddWithValue("@CountryID", CountryID);
+
+        try
+        {
+            connection.Open();
+            SqlDataReader reader = command.ExecuteReader();
+            while (reader.Read())
+            {
+                int contactID = (int)reader["ContactID"];
+                string firstName = (string)reader["FirstName"];
+                string lastName = (string)reader["LastName"];
+                string email = (string)reader["Email"];
+                string phone = (string)reader["Phone"];
+                string address = (string)reader["Address"];
+                int countryID = (int)reader["CountryID"];
+
+                Console.WriteLine($"Contact ID: {contactID}");
+                Console.WriteLine($"Name: {firstName} {lastName}");
+                Console.WriteLine($"Email: {email}");
+                Console.WriteLine($"Phone: {phone}");
+                Console.WriteLine($"Address: {address}");
+                Console.WriteLine($"Country ID: {countryID}");
+                Console.WriteLine();
+
+            }
+            reader.Close();
+            connection.Close();
+        }
+        catch (Exception e)
+        {
+            Console.WriteLine("error ==>> " + e.Message);
+        }
+
+    }
+
+    static void ParameterizedQueryUnSave(string FirstName, int CountryID)
+    {
+        SqlConnection connection = new SqlConnection(connectionString);
+        //string query = "select * from Contacts where Contacts.FirstName=@FirstName";
+        string query2 = "select * from Contacts where Contacts.FirstName='" + FirstName + "' and Contacts.CountryID=" + CountryID + ";";
+        Console.WriteLine(query2);
+        SqlCommand command = new SqlCommand(query2, connection);
+        try
+        {
+            connection.Open();
+            SqlDataReader reader = command.ExecuteReader();
+            while (reader.Read())
+            {
+                int contactID = (int)reader["ContactID"];
+                string firstName = (string)reader["FirstName"];
+                string lastName = (string)reader["LastName"];
+                string email = (string)reader["Email"];
+                string phone = (string)reader["Phone"];
+                string address = (string)reader["Address"];
+                int countryID = (int)reader["CountryID"];
+
+                Console.WriteLine($"Contact ID: {contactID}");
+                Console.WriteLine($"Name: {firstName} {lastName}");
+                Console.WriteLine($"Email: {email}");
+                Console.WriteLine($"Phone: {phone}");
+                Console.WriteLine($"Address: {address}");
+                Console.WriteLine($"Country ID: {countryID}");
+                Console.WriteLine();
+
+            }
+            reader.Close();
+            connection.Close();
+        }
+        catch (Exception e)
+        {
+            Console.WriteLine("error ==>> " + e.Message);
+        }
+
+    }
+
+
     public static void Main()
     {
-        PrintAllContacts2();
-
+        //PrintAllContacts2();
+        //ParameterizedQuery("john", 1);
+        ParameterizedQuery("john' and Contacts.LastName='Doe", 1);
+        //ParameterizedQueryUnSave("john' and Contacts.LastName='Doe", 1);
         Console.ReadKey();
     }
 
