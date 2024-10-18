aca17403d4d49cb5b83638a8b955a6ab69e24399
Ahmed Mady <96249012+ahmed-eid-faried@users.noreply.github.com>
2024-10-18 12:35:54 +0300

    ParameterizedQueryWithLike

--- db-connectivity-in-csharp/Program.cs
+++ db-connectivity-in-csharp/Program.cs
@@ -53,7 +53,6 @@ public class Program
 
 
     }
-
     static void PrintAllContacts2()
     {
         SqlConnection connection = new SqlConnection(connectionString);
@@ -93,7 +92,6 @@ public class Program
 
 
     }
-
     static void ParameterizedQuery(string FirstName, int CountryID)
     {
         SqlConnection connection = new SqlConnection(connectionString);
@@ -176,13 +174,142 @@ public class Program
 
     }
 
+    static void SearchContactsStartsWith(string StartsWith)
+    {
+        SqlConnection connection = new SqlConnection(connectionString);
+        string query = "SELECT * FROM Contacts WHERE FirstName LIKE '' + @StartsWith +'%'";
+
+        SqlCommand command = new SqlCommand(query, connection);
+        command.Parameters.AddWithValue("@StartsWith", StartsWith);
+        try
+        {
+            connection.Open();
+            SqlDataReader reader = command.ExecuteReader();
+            while (reader.Read())
+            {
+                int contactID = (int)reader["ContactID"];
+                string firstName = (string)reader["FirstName"];
+                string lastName = (string)reader["LastName"];
+                string email = (string)reader["Email"];
+                string phone = (string)reader["Phone"];
+                string address = (string)reader["Address"];
+                int countryID = (int)reader["CountryID"];
+
+                Console.WriteLine($"Contact ID: {contactID}");
+                Console.WriteLine($"Name: {firstName} {lastName}");
+                Console.WriteLine($"Email: {email}");
+                Console.WriteLine($"Phone: {phone}");
+                Console.WriteLine($"Address: {address}");
+                Console.WriteLine($"Country ID: {countryID}");
+                Console.WriteLine();
+
+            }
+            reader.Close();
+            connection.Close();
+        }
+        catch (Exception e)
+        {
+            Console.WriteLine("error ==>> " + e.Message);
+        }
+
+    }
+
+    static void SearchContactsEndsWith(string EndsWith)
+    {
+        SqlConnection connection = new SqlConnection(connectionString);
+        string query = "SELECT * FROM Contacts WHERE FirstName LIKE '%' + @EndsWith +'';";
+
+        SqlCommand command = new SqlCommand(query, connection);
+        command.Parameters.AddWithValue("@EndsWith", EndsWith);
+        try
+        {
+            connection.Open();
+            SqlDataReader reader = command.ExecuteReader();
+            while (reader.Read())
+            {
+                int contactID = (int)reader["ContactID"];
+                string firstName = (string)reader["FirstName"];
+                string lastName = (string)reader["LastName"];
+                string email = (string)reader["Email"];
+                string phone = (string)reader["Phone"];
+                string address = (string)reader["Address"];
+                int countryID = (int)reader["CountryID"];
+
+                Console.WriteLine($"Contact ID: {contactID}");
+                Console.WriteLine($"Name: {firstName} {lastName}");
+                Console.WriteLine($"Email: {email}");
+                Console.WriteLine($"Phone: {phone}");
+                Console.WriteLine($"Address: {address}");
+                Console.WriteLine($"Country ID: {countryID}");
+                Console.WriteLine();
+
+            }
+            reader.Close();
+            connection.Close();
+        }
+        catch (Exception e)
+        {
+            Console.WriteLine("error ==>> " + e.Message);
+        }
+
+    }
+
+    static void SearchContactsContainsWith(string Contains)
+    {
+        SqlConnection connection = new SqlConnection(connectionString);
+        string query = "SELECT * FROM Contacts WHERE FirstName LIKE '%' + @Contains +'%';";
+
+        SqlCommand command = new SqlCommand(query, connection);
+        command.Parameters.AddWithValue("@Contains", Contains);
+        try
+        {
+            connection.Open();
+            SqlDataReader reader = command.ExecuteReader();
+            while (reader.Read())
+            {
+                int contactID = (int)reader["ContactID"];
+                string firstName = (string)reader["FirstName"];
+                string lastName = (string)reader["LastName"];
+                string email = (string)reader["Email"];
+                string phone = (string)reader["Phone"];
+                string address = (string)reader["Address"];
+                int countryID = (int)reader["CountryID"];
+
+                Console.WriteLine($"Contact ID: {contactID}");
+                Console.WriteLine($"Name: {firstName} {lastName}");
+                Console.WriteLine($"Email: {email}");
+                Console.WriteLine($"Phone: {phone}");
+                Console.WriteLine($"Address: {address}");
+                Console.WriteLine($"Country ID: {countryID}");
+                Console.WriteLine();
+
+            }
+            reader.Close();
+            connection.Close();
+        }
+        catch (Exception e)
+        {
+            Console.WriteLine("error ==>> " + e.Message);
+        }
+
+    }
+    static void ParameterizedQueryWithLike(){
+        Console.WriteLine("=====================SearchContactsStartsWith j==============");
+        SearchContactsStartsWith("j");
+        Console.WriteLine("=====================SearchContactsEndsWith e==============");
+        SearchContactsEndsWith("e");
+        Console.WriteLine("=====================SearchContactsContainsWith ae==============");
+        SearchContactsContainsWith("ae");
+        Console.WriteLine("================================================================");
+    }
 
     public static void Main()
     {
         //PrintAllContacts2();
         //ParameterizedQuery("john", 1);
-        ParameterizedQuery("john' and Contacts.LastName='Doe", 1);
+        //ParameterizedQuery("john' and Contacts.LastName='Doe", 1);
         //ParameterizedQueryUnSave("john' and Contacts.LastName='Doe", 1);
+        ParameterizedQueryWithLike();
         Console.ReadKey();
     }
 
